2b730287d1c22f355a73bb0c09ea732d95e85489
nicolas-f <github@nettrader.fr>
2014-10-19 15:10:45 +0200

    take account of mixed herbs

--- sql_test.py
+++ sql_test.py
@@ -30,16 +30,51 @@ def main(data_folder):
         ## Init known effects
         st.execute("DROP TABLE IF EXISTS known")
         st.execute("CREATE TABLE known(ide INTEGER, idh INTEGER, PRIMARY KEY (ide, idh))")
-        with open(data_folder+"/effects_found.csv") as f:
+        # with open(data_folder+"/effects_found.csv") as f:
+        #     for line in f:
+        #         st.execute("INSERT INTO known(idh, ide) VALUES (?, ?)", fetch_herb_effect(st, line[:-1].split(",")))
+
+
+        ## Load mix history
+        st.execute("DROP TABLE IF EXISTS mix")
+        st.execute("CREATE TABLE mix(idm INTEGER PRIMARY KEY, failed boolean)")
+        st.execute("DROP TABLE IF EXISTS mix_herbs")
+        st.execute("CREATE TABLE mix_herbs(idm INTEGER, idh INTEGER,failed boolean, PRIMARY KEY(idm,idh))")
+        with open(data_folder+"/mix.csv") as f:
             for line in f:
-                st.execute("INSERT INTO known(idh, ide) VALUES (?, ?)", fetch_herb_effect(st, line[:-1].split(",")))
+                failed = False
+                herbs_effects = line[:-1].split(";")
+                for effect in herbs_effects[1].split(","):
+                    if effect == "-1":
+                        failed = True
+                idmix = st.execute("INSERT INTO mix values(null, ?)", [failed]).lastrowid
+                for herb in herbs_effects[0].split(","):
+                    st.execute("INSERT INTO mix_herbs(idm, idh) " +
+                               "VALUES (?,(select idh from herbs where name=?))", [idmix, herb])
+                    for effect in herbs_effects[1].split(","):
+                        if effect != "-1":
+                            st.execute("INSERT INTO known(idh, ide) VALUES ((select idh from herbs where name=?),"
+                                       " (select ide from effects where name=?))", [herb, effect])
+
 
         ## Remove unknown effects using known effects
         st.execute("DELETE FROM unknown WHERE ide IN (select known.ide from known where known.idh = unknown.idh)")
-
-
-        for line in st.execute("SELECT h.name, e.name from unknown u, herbs h, effects e "
+        ## Remove unknown effects of B using failed mix of herb A and B where A.effect is known
+        res = st.execute("DELETE FROM unknown WHERE ide IN "
+                   "(SELECT known.ide FROM known, mix_herbs WHERE known.idh = mix_herbs.idh AND mix_herbs.idm IN"
+                   "(SELECT mix.idm FROM mix,mix_herbs mh WHERE mix.failed AND mix.idm = mh.idm"
+                   " AND mh.idh = unknown.idh))")
+        ##Add known effects if unknown effects is reduced to missing slots
+        st.execute("insert into known SELECT uk.ide,uk.idh from unknown uk where uk.idh IN (SELECT herbs.idh from herbs"
+                   " WHERE 3 - (select count(*) from known k where k.idh = herbs.idh) - (select count(*) "
+                   "from unknown u where u.idh = herbs.idh) = 0)")
+        print "Remove unknown "+str(res.rowcount)
+        print "Known"
+        for line in st.execute("SELECT h.name, e.name from known u, herbs h, effects e "
                                "where u.idh = h.idh and u.ide=e.ide order by h.name, e.name"):
             print line[0], line[1]
-
+        # print "Unknown"
+        # for line in st.execute("SELECT h.name, e.name from unknown u, herbs h, effects e "
+        #                        "where u.idh = h.idh and u.ide=e.ide order by h.name, e.name"):
+        #     print line[0], line[1]
 main("data_test")
